a6f2e3f248d6682db41826c72092ad3a92c04a20
unknown <alpatovdanila@gmail.com>
2020-06-18 23:12:03 +0300

    gh-pages

--- src/routing/constructSearchRoutePath.js
+++ src/routing/constructSearchRoutePath.js
@@ -1,7 +1,4 @@
 //@flow
-
-
-
 export const constructSearchRoutePath = ({query, page, sort, order, lang}) => {
     let url = `/search/?query=${query ? query : ''}`;
     if(page) url+=`&page=${page}`;
